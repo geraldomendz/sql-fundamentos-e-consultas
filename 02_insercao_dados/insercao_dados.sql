55556e26161c71e3af30d7e0d3fdd365ac43feee
geraldomendz <geraldomendes3000@gmail.com>
2026-01-10 11:31:24 -0300

    Adicionando query de inserção de dados

--- 02_insercao_dados/insercao_dados.sql
+++ 02_insercao_dados/insercao_dados.sql
@@ -1,1 +1,34 @@
 
+-- Adiciona um único produto usando INSERT simples 
+
+INSERT INTO tb_produto 
+(codigo,nome,embalagem,tamanho,sabor,preco_lista) 
+VALUES 
+('1040107','Light','Lata','350 ml','Melância',4.56);
+
+
+-- Adiciona vários produtos usando INSERT múltiplo 
+
+INSERT INTO tb_produto 
+(codigo,nome,embalagem,tamanho,sabor,preco_lista) 
+VALUES 
+('1037797','Clean','PET','2 Litros','Laranja',16.01),
+('1000889','Sabor da Montanha','Garrafa','700 ml','Uva',7.00),
+('1088126','Linha Citros','PET','1 Litro ','Limão',6.31),
+('1004327','Videira do Campo','PET','1,5 Litros','Melância',19.51);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
